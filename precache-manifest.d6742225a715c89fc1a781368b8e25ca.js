14cbc0d2f1374332391d49286b1e26ba2c25200e
Code Sandbox <hello@codesandbox.io>
2021-03-22 00:12:22 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d6742225a715c89fc1a781368b8e25ca.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14265c580038e5afed6a4c83acc14bf",
+    "url": "https://AmySofia1451.github.io/csb-1gmtt/index.html"
+  },
+  {
+    "revision": "a48f9d490ac6c260b2fd",
+    "url": "https://AmySofia1451.github.io/csb-1gmtt/static/js/2.41d2c938.chunk.js"
+  },
+  {
+    "revision": "2a01004d5cac1ac04284",
+    "url": "https://AmySofia1451.github.io/csb-1gmtt/static/js/main.683354da.chunk.js"
+  },
+  {
+    "revision": "9ddd9cdddc64de8ef321",
+    "url": "https://AmySofia1451.github.io/csb-1gmtt/static/js/runtime-main.3d23f6a1.js"
+  }
+]);
